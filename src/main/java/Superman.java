04fdefcef31ef50c5e51d56c40d3611b09098f7e
hafizeakyildiz <hafizeakyildiz1@gmail.com>
2020-11-01 11:01:43 -0500

    changes

--- src/main/java/Superman.java
+++ src/main/java/Superman.java
@@ -1,5 +1,7 @@
 public class Superman {
     public static void main(String[] args) {
         System.out.println("superman");
+        System.out.println("making changes from gotham branch");
+        System.out.println("makin changes from master branch");
     }
 }
--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -1,5 +1,6 @@
 public class Batman {
     public static void main(String[] args) {
-
+        System.out.println("making changes from gotham branch");
+        System.out.println("making changes from master branch");
     }
 }
